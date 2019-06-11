275ac783b00fae6bc9982db2d42ebb118a9928de
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-11 10:18:41 -0500

    Camel Case

--- HolaMundoJava/src/NamingJava.java
+++ HolaMundoJava/src/NamingJava.java
@@ -1,3 +1,4 @@
+             //Upper Camel Case
 public class NamingJava {
     public static void main(String[] args) {
         int celphone = 33337777;
@@ -15,7 +16,10 @@ public class NamingJava {
         int MAX_WIDTH = 9999;
         int MIN_WIDTH = 1;
 
-        
+        //Lower Camel Case
+        String fullName = "Irene Anahi Salgado Díaz de la Vega";
+        int sizeInCentimeters = 26;
+
 
     }
 }
