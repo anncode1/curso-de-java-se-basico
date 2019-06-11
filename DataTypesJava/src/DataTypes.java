a3bebce9ef35521925eb3438b913d798a452da2e
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-11 11:06:11 -0500

    Numbers Data Types

---
+++ DataTypesJava/src/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int n = 1234567890;
+        long nL = 12345678901L;
+
+        double nD = 123.456;
+        float nF = 123.456F;
+
+
+    }
+}
