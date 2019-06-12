eadde1a59ef0fa50184ebd9618b288b63afb83cf
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-12 11:17:17 -0500

    Switch

---
+++ HolaMundoJava/src/SwitchStatement.java
@@ -0,0 +1,27 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+
+        String colorModeSelected = "Dark";
+
+        switch (colorModeSelected){
+            case "Light":
+                System.out.println("Seleccionaste Light Mode");
+                break;
+            case "Night": //Ambar
+                System.out.println("Seleccionaste Night Mode");
+                break;
+            case "Blue Dark":
+                System.out.println("Seleccionaste Blue Dark Mode");
+                break;
+            case "Dark":
+                System.out.println("Seleccionaste Dark Mode");
+                break;
+
+            default:
+                System.out.println("Selecciona una opción");
+
+
+        }
+
+    }
+}
