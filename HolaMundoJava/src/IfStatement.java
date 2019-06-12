a0ac1944c45b45e07788331d3c2cb4edd12123d5
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-12 10:21:46 -0500

    If statement

---
+++ HolaMundoJava/src/IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluetoothEnabled = false;
+        int fileSended = 3;
+
+        if (isBluetoothEnabled){
+            //Send file
+            fileSended++;
+            System.out.println("Archivo Enviado");
+        }
+    }
+}
