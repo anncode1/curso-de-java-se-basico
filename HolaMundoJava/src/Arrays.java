5083b5b0d7e0d3aa22b4b24b56176f0b539b26bd
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-13 10:08:45 -0500

    Arrays

---
+++ HolaMundoJava/src/Arrays.java
@@ -0,0 +1,24 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2]; //4 * 2 = 8
+        /*
+         * +-------------------------+
+         * | Country   |  City       |
+         * ---------------------------
+         * | México    | CDMX        |
+         * | Méxicp    | Guadalajara |
+         * | Colombia  | Bogotá      |
+         * | Colombia  | Medellín    |
+         * +-------------------------+
+         * */
+
+        int [][][] numbers = new int[2][2][2];
+        int [][][][] numbers4 = new int[2][2][2][2];
+
+
+
+    }
+}
