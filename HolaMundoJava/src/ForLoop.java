ea86039776d0feec347075d3ae5fa1c39e8aa5c2
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-13 09:22:51 -0500

    For

---
+++ HolaMundoJava/src/ForLoop.java
@@ -0,0 +1,22 @@
+public class ForLoop {
+
+    static boolean isTurnOnLight = false;
+
+    public static void main(String[] args) {
+        turnOnOffLight();
+
+        for (int i = 1; i <= 10; i++) {
+            printSOS();
+        }
+
+    }
+
+    public static void printSOS(){
+        System.out.println(". . . _ _ _ . . .");
+    }
+
+    public static boolean turnOnOffLight(){
+        isTurnOnLight = (isTurnOnLight)?false:true;
+        return isTurnOnLight;
+    }
+}
