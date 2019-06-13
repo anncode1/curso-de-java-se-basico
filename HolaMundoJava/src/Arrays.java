8ee7ace25e8508281f4e6c0b726a66abeab99384
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-13 11:17:48 -0500

    For y Foreach anidados

--- HolaMundoJava/src/Arrays.java
+++ HolaMundoJava/src/Arrays.java
@@ -1,7 +1,7 @@
 public class Arrays {
     public static void main(String[] args) {
         String[] androidVersions = new String[17];
-        String days[] = new String[7];
+        int days[] = new int[7];
 
         String[][] cities = new String[4][2]; //4 * 2 = 8
         /*
@@ -9,7 +9,7 @@ public class Arrays {
          * | Country   |  City       |
          * ---------------------------
          * | México    | CDMX        |
-         * | Méxicp    | Guadalajara |
+         * | México    | Guadalajara |
          * | Colombia  | Bogotá      |
          * | Colombia  | Medellín    |
          * +-------------------------+
@@ -23,10 +23,19 @@ public class Arrays {
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donut";
 
-        System.out.println(androidVersions[0]);
+                                    //17
+        /*for (int i = 0; i < androidVersions.length; i++) {
+            System.out.println(androidVersions[i]);
+        }*/
+
+        for (String androidVersion : androidVersions) {
+            System.out.println(androidVersion);
+        }
+
+        /*System.out.println(androidVersions[0]);
         System.out.println(androidVersions[1]);
         System.out.println(androidVersions[2]);
-        System.out.println(androidVersions[3]);
+        System.out.println(androidVersions[3]);*/
 
 
         System.out.println();
@@ -42,14 +51,26 @@ public class Arrays {
         cities[3][1] = "CDMX";
 
 
-        System.out.println(cities[0][0]);
+        /*System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
         System.out.println(cities[1][1]);
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
+
+        /*for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }*/
+
+        for (String[] pair : cities) {
+            for ( String name :pair) {
+                System.out.println(name);
+            }
+        }
 
 
         String[][][][] animals = new String[2][3][2][2];
@@ -58,6 +79,16 @@ public class Arrays {
         System.out.println();
         System.out.println();
         System.out.println(animals[1][0][0][1]);
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <=0 ; j++) {
+                for (int k = 0; k <= 0 ; k++) {
+                    for (int l = 0; l <= 1 ; l++) {
+                        System.out.println(animals[i][j][k][l]);
+                    }
+                }
+            }
+        }
 
 
 
