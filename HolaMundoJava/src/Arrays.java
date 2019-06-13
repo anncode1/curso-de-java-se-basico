95e8cf2f9fcec7ef372f7638fa56cf0091be5155
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-13 10:54:43 -0500

    Arrays Data Insert

--- HolaMundoJava/src/Arrays.java
+++ HolaMundoJava/src/Arrays.java
@@ -18,6 +18,49 @@ public class Arrays {
         int [][][] numbers = new int[2][2][2];
         int [][][][] numbers4 = new int[2][2][2][2];
 
+        androidVersions[0] = "Apple Pie";
+        androidVersions[1] = "Bannana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);
+
+
+        System.out.println();
+        System.out.println();
+
+        cities[0][0] = "Colombia";
+        cities[0][1] = "Medellín";
+        cities[1][0] = "Colombia";
+        cities[1][1] = "Bogotá";
+        cities[2][0] = "México";
+        cities[2][1] = "Guadalajara";
+        cities[3][0] = "México";
+        cities[3][1] = "CDMX";
+
+
+        System.out.println(cities[0][0]);
+        System.out.println(cities[0][1]);
+        System.out.println(cities[1][0]);
+        System.out.println(cities[1][1]);
+        System.out.println(cities[2][0]);
+        System.out.println(cities[2][1]);
+        System.out.println(cities[3][0]);
+        System.out.println(cities[3][1]);
+
+
+        String[][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1] = "Monkey";
+
+        System.out.println();
+        System.out.println();
+        System.out.println(animals[1][0][0][1]);
+
+
+
 
 
     }
